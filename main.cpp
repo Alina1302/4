309f813b4199159bc662b005463f89810dcac6ae
Alina1302 <122560010+Alina1302@users.noreply.github.com>
2023-02-14 20:38:34 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,20 +1,29 @@
-//Тевс Алина Вячеславовна
 /*
-Задание 1. Дана строка символов. Заменить в ней все пробелы на табуляции.
- */
+ * Задание 2. Дана строка символов.
+ * Определить количество букв, цифр и остальных символов, присутствующих в строке.*/
 #include <iostream>
 #include <string>
 using namespace std;
-int main() {
-    string source = "Hi my dear friend !";
-    string _old = " ";
-    string _new = "\t";
-    int pos;
-    do {
-        pos = source.find(_old);
-        if (pos < 0 ) break;
-        source.replace(pos, _new.size(), _new);
-    } while (pos >= 0);
-    cout << source << endl;
+
+int main(){
+    string _string;
+    cin >> _string;
+    int letters=0, digit = 0, symbol=0;
+    int b = _string.size();
+    cout << b<< endl;
+    for (int i=0; i< b; i++){
+        if (isalpha(_string[i])){
+            letters++;
+        }else{
+            if(isdigit(_string [i])){
+                digit++;
+            }else{
+                symbol++;
+            }
+        }
+    }
+    cout << "Quantity of letters = "<<letters<<endl;
+    cout << "Quantity of digit = "<<digit<<endl;
+    cout << "Quantity of symbols = "<<symbol<<endl;
     return 0;
 }
